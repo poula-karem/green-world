e861b6b551950e3bf53c5f07ae6c75d2f712b821
Eric <ericgeorge4477@gmail.com>
2024-12-14 15:52:58 +0200

    Add responsive styles and background image to AuthPage

--- frontend/src/pages/AuthPage/AuthPage.tsx
+++ frontend/src/pages/AuthPage/AuthPage.tsx
@@ -1,10 +1,13 @@
 import SignUpForm from "./SignUpForm";
 import logo from "/public/logo.svg";
+import styles from "./AuthPage.module.css";
 
 export default function AuthPage() {
   return (
-    <div className="bg-gradient-to-br from-[#b5d4b5] to-[#a1c4a1] py-4 flex items-center justify-center min-h-screen shadow-xl">
-      <div className="bg-white py-4 px-7 w-[40%] max-w-[700px] rounded-md overflow-y-auto  max-h-[95vh] scrollbar-hidden">
+    <div
+      className={`py-4 flex items-center justify-center min-h-screen shadow-xl  ${styles.backgroundImage}`}
+    >
+      <div className="bg-white py-4 px-7 w-[40%] max-lg:w-[60%] max-md:w-[70%] max-sm:w-[90%] max-w-[700px] rounded-md overflow-y-auto max-h-[95vh]">
         <div className=" flex justify-center">
           <div className="logo w-[100px] h-[100px]">
             <img className="w-full h-full" src={logo} alt="logo" />
